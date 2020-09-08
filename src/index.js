464a2ce384fb9b69018464fad34cc4711ad62578
John Ahn <smileajw1004@gmail.com>
2020-09-08 17:56:54 +0900

    fix conflicts

--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom';
 import RouterPage from "./components/router";
 import './assets/css/index.css';
 import { CLIENT_RENEG_WINDOW } from 'tls';
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducer";
-
-const store = createStore(rootReducer);
-const { remote } = window.require("electron")
-
-import Reducer from './redux/reducers';
+import Reducer from './reducer';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
-
+// const { remote } = window.require("electron")
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
@@ -30,15 +23,8 @@ ReactDOM.render(
     </Provider>
     , document.getElementById('root'));
 
-
-ReactDOM.render(
-    <Provider store={store}>
-        <RouterPage />
-    </Provider>,
-    document.getElementById('root'));
-
 // Dev Mode
-if (remote.getGlobal('IS_DEV')) {
-    sessionStorage.setItem('isLoginElectronApp', true)
-}
+// if (remote.getGlobal('IS_DEV')) {
+//     sessionStorage.setItem('isLoginElectronApp', true)
+// }
 
